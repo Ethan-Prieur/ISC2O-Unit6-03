9c5496f332a7b0a5a0c1badbf11a227d770ffda0
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-04 14:43:54 +0000

    please work

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.getElementById('btn').addEventListener('click', myFunction)
 function myFunction () {
   let age = document.getElementById('box').value
   age = parseInt(age)
-  if (age > = 17) {
+  if (age > 16) {
     alert('you can see an R rated movie alone');
-  } else if (age > = 13) {
+  } else if (age > 12) {
     alert('you can see a pg 13 movie alone')
-  } else if (age > = 5) {
+  } else if (age > 4) {
     alert('you can see a G or PG movie alone')
   } else {
     alert("Sorry you're too young for these things")
