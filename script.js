7b9889493c6398f62b4a1b33da06caabb75da987
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-04 14:37:29 +0000

    More Minor errors

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.getElementById('btn').addEventListener('click', myFunction)
 function myFunction () {
   let age = document.getElementById('box').value
   age = parseInt(age)
-if (age >=17) {
-  alert("you can see an R rated movie alone");
-} else if(age >=13) {
-  alert("you can see a pg 13 movie alone")
-} else if (age >=5) {
-  alert("you can see a G or PG movie alone")
-} else {
-  alert("Sorry you're too young for these things")
-}
+  if (age > = 17) {
+    alert('you can see an R rated movie alone');
+  } else if (age > = 13) {
+    alert('you can see a pg 13 movie alone')
+  } else if (age > = 5) {
+    alert('you can see a G or PG movie alone')
+  } else {
+    alert("Sorry you're too young for these things")
+  }
 }
